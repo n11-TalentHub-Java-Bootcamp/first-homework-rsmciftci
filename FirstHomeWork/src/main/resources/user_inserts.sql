cf04668946275ec4f2c86e8b32d079baa248c347
Rasim ÇİFTÇİ <62359863+rsmciftci@users.noreply.github.com>
2021-12-14 22:20:07 +0300

    Delete FirstHomeWork directory

--- FirstHomeWork/src/main/resources/user_inserts.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public.USERS ( ID, EMAIL, NAME, PHONE_NUMBER, SURNAME ) VALUES ( 1,'rsmciftci@gmail.com','Rasim','0541','Ciftci' );
-INSERT INTO public.USERS ( ID, EMAIL, NAME, PHONE_NUMBER, SURNAME ) VALUES ( 2,'noname2@gmail.com','noname2','0542','nosurname2' );
-INSERT INTO public.USERS ( ID, EMAIL, NAME, PHONE_NUMBER, SURNAME ) VALUES ( 3,'noname3@gmail.com','noname3','0543','nosurname3' );
-INSERT INTO public.USERS ( ID, EMAIL, NAME, PHONE_NUMBER, SURNAME ) VALUES ( 4,'noname4@gmail.com','noname4','0544','nosurname4' );
--- FirstHomeWork/src/test/java/entityservice/KategoriDao.java
+++
@@ -1,4 +0,0 @@
-package entityservice;
-
-public class KategoriDao {
-}
